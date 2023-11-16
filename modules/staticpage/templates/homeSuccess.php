bfe52c379da5f69832e59f5a838edd3ed76b6f74
Marcelo Alencar <marceloalencar@users.noreply.github.com>
2023-11-16 17:58:28 -0300

    de hoje

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -11,17 +11,22 @@
 
 <?php end_slot(); ?>
 
-<div class="mb-3">
-  <div>
-    <p>UFPA: MAIS QUE UMA UNIVERSIDADE</p>
-    <p>A Universidade Federal do Pará está presente em 78 municípios paraenses com atividades vinculadas a 12 campi: Abaetetuba, Altamira, Ananindeua, Belém, Bragança, Breves, Cametá, Capanema, Castanhal, Salinópolis, Soure e Tucuruí.</p>
+<section id="apresentacao-ufpa" class="mb-3">
+  <div class="py-3">
+    <h1>UFPA: MAIS QUE UMA UNIVERSIDADE</h1>
+    <p class="descricao"><br></p>
+    <p class="descricao">A Universidade Federal do Pará está presente em 78 municípios paraenses</p>
+    <p class="descricao">com atividades vinculadas a 12 <em>campi</em>: Abaetetuba, Altamira, Ananindeua, Belém, Bragança,</p>
+    <p class="descricao">Breves, Cametá, Capanema, Castanhal, Salinópolis, Soure e Tucuruí.</p>
   </div>
-  <div><p><span>5.379</span></p><p><span>DOCENTES E TÉCNICOS</span></p></div>
-  <div><p><span>50.374</span></p><p><span>ALUNOS</span></p></div>
-  <div><p><span>155</span></p><p><span>CURSOS DE GRADUAÇÃO</span></p></div>
-  <div><p><span>144</span></p><p><span>CURSOS DE PÓS-GRADUAÇÃO</span></p></div>
-  <div><p><span>56</span></p><p><span>CURSOS DE RESIDÊNCIA E ESPECIALIZAÇÃO</span></p></div>
-</div>
+  <div class="py-3">
+    <div><p><span>5.379</span></p><p><span>DOCENTES E TÉCNICOS</span></p></div>
+    <div><p><span>50.374</span></p><p><span>ALUNOS</span></p></div>
+    <div><p><span>155</span></p><p><span>CURSOS DE GRADUAÇÃO</span></p></div>
+    <div><p><span>144</span></p><p><span>CURSOS DE PÓS-GRADUAÇÃO</span></p></div>
+    <div><p><span>56</span></p><p><span>CURSOS DE RESIDÊNCIA E ESPECIALIZAÇÃO</span></p></div>
+  </div>
+</section>
 
 <?php $browseMenu = QubitMenu::getById(QubitMenu::BROWSE_ID); ?>
 <?php if ($browseMenu->hasChildren()) { ?>
