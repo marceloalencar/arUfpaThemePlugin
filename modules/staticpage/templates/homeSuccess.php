e3fc6d35eda75bd3a8fe52e77414d2fe9ae78cd9
Marcelo Alencar <marceloalencar@users.noreply.github.com>
2023-11-17 13:21:50 -0300

    banner numeros

--- modules/staticpage/templates/homeSuccess.php
+++ modules/staticpage/templates/homeSuccess.php
@@ -12,19 +12,19 @@
 <?php end_slot(); ?>
 
 <section id="apresentacao-ufpa" class="mb-3">
-  <div class="py-3">
+  <div class="d-flex flex-column align-items-center px-3 py-3">
     <h1>UFPA: MAIS QUE UMA UNIVERSIDADE</h1>
     <p class="descricao"><br></p>
     <p class="descricao">A Universidade Federal do Pará está presente em 78 municípios paraenses</p>
     <p class="descricao">com atividades vinculadas a 12 <em>campi</em>: Abaetetuba, Altamira, Ananindeua, Belém, Bragança,</p>
     <p class="descricao">Breves, Cametá, Capanema, Castanhal, Salinópolis, Soure e Tucuruí.</p>
   </div>
-  <div class="py-3">
-    <div><p><span>5.379</span></p><p><span>DOCENTES E TÉCNICOS</span></p></div>
-    <div><p><span>50.374</span></p><p><span>ALUNOS</span></p></div>
-    <div><p><span>155</span></p><p><span>CURSOS DE GRADUAÇÃO</span></p></div>
-    <div><p><span>144</span></p><p><span>CURSOS DE PÓS-GRADUAÇÃO</span></p></div>
-    <div><p><span>56</span></p><p><span>CURSOS DE RESIDÊNCIA E ESPECIALIZAÇÃO</span></p></div>
+  <div class="d-flex flex-row flex-wrap justify-content-center px-3 py-3">
+    <div class="numeros flex-fill"><p>5.379</p><p>DOCENTES E TÉCNICOS</p></div>
+    <div class="numeros flex-fill"><p>50.374</p><p>ALUNOS</p></div>
+    <div class="numeros flex-fill"><p>155</p><p>CURSOS DE GRADUAÇÃO</p></div>
+    <div class="numeros flex-fill"><p>144</p><p>CURSOS DE</p><p>PÓS-GRADUAÇÃO</p></div>
+    <div class="numeros flex-fill"><p>56</p><p>CURSOS DE</p><p>RESIDÊNCIA E ESPECIALIZAÇÃO</p></div>
   </div>
 </section>
 
